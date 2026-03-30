7930d05f9fb83eab8f9ca8a60660f668fbc54b90
karthiksai3031-lab <karthik.sai3031@gmail.com>
2026-03-30 21:45:54 +0530

    created uc4

--- HelloApp.java
+++ HelloApp.java
@@ -2,16 +2,19 @@ public class HelloApp {
 
     public static void main(String[] args) {
 
-        // Check if no arguments were provided
+        // Check if names are provided
         if (args.length == 0) {
-            System.out.println("Hello, World!");
+            System.out.println("Hello World");
         } else {
-            // Enhanced for loop to process multiple names
+
+            // Loop through all names
             for (String name : args) {
-                System.out.println("Hello, " + name + "!");
+                System.out.println("Hello " + name);
             }
+
         }
 
     }
 
 }
+ 
